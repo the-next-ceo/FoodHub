b1a87bbd0af1c7ef76f47e75b15705de1fe2c420
the-next-ceo <kshitij1998@gmail.com>
2022-09-26 01:55:41 +0530

    added gif

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,12 @@ function App() {
   return (
     
    <div className="App">
-    {/* <span>{Navbar()}</span> */}
-    
+
     <span>{foodHub()}</span>
-    {/* <span className="tenor-gif-embed" data-postid="6079735" data-share-method="host" data-aspect-ratio="2.06612" data-width="100%"><a href="https://tenor.com/view/okonomiyaki-japanese-food-anime-food-gif-6079735">Okonomiyaki Japanese Food GIF</a>from <a href="https://tenor.com/search/okonomiyaki-gifs">Okonomiyaki GIFs</a></span> <script type="text/javascript" async src="https://tenor.com/embed.js"></script> */}
-    <img src={require('./gif/food.gif')} alt="food" className='image'/>
+    <img width={1000} src={require('./gif/hello.gif')} alt="food" className='image'/>
+    {/* <video   autoPlay = {true}  loop muted playsInline className='video'>
+      <source src="./gif/new.mp4" type="video/mp4" />
+    </video> */}
 
 
    </div> 
